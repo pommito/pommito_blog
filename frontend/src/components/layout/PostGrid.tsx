c67a50708c947df80fcbf373381b48ff45a9bab1
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-06-17 18:57:00 +0200

    sport break, will finish the static implementation later

--- frontend/src/components/layout/PostGrid.tsx
+++ frontend/src/components/layout/PostGrid.tsx
@@ -2,7 +2,10 @@ import Card from '../common/Card';
 
 export default function PostGrid() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-12">
+      <Card />
+      <Card />
+      <Card />
       <Card />
       <Card />
       <Card />
